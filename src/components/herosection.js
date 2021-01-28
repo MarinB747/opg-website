d052e5a62551d86a2c0dededd51d05f9203b1674
Marin Blazevic <blazevic.marin747@gmail.com>
2021-01-28 16:59:48 +0100

    owerflow-x i menu bar

--- src/components/herosection.js
+++ src/components/herosection.js
@@ -5,7 +5,7 @@ import "./herosection.css"
 function HeroSection() {
   return (
     <div className="hero-container">
-      <video src="/videos/video-2.mp4" autoPlay loop muted/>
+       <video src="/videos/video-2.mp4" autoPlay loop muted/> 
       <h1>Nekakav slogan</h1>
       <p></p>
       </div>
