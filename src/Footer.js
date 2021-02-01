ab97ed783ca53709099b1bea81f160c101d65c2e
Marin Blazevic <blazevic.marin747@gmail.com>
2021-02-01 19:19:21 +0100

    to bi bilo to

--- src/Footer.js
+++ src/Footer.js
@@ -1,11 +1,67 @@
 import React from 'react'
-import { Button, onClick } from "./components/button" 
 import "./Footer.css"
-import "./components/button";
-import sendEmail from "./components/email"
+import emailjs from "emailjs-com";
+import{ init } from 'emailjs-com';
+init("user_aA2UgjKNawzlffIXMWDFo");
 
-function Footer() {
+export default function Footer(){
+  function sendEmail(e) {
+    e.preventDefault();
   
+  emailjs.sendForm('service_nayx53a', 'template_fjkj6ko', e.target, 'user_aA2UgjKNawzlffIXMWDFo')
+    .then((result) => {
+    console.log(result.text);
+    }, (error) => {
+    console.log(error.text);
+    });
+    e.target.reset();
+}
+    return(
+    <div className="footer-container">
+      <section className="footer-subscription">
+        <p className="footer-subscription-heading">
+        Kontakt
+        </p>
+        <p className="footer-subscription-text">     
+        Ukoliko imate bilo kakvih upita slobodno nam se obratite putem naše kontakt forme 
+        </p>
+        <div className="input-areas">
+          <form onSubmit={sendEmail}>
+            <label>
+            <input type="text" name="name" placeholder="Puno ime" className="footer-input" />
+            </label>
+            <label>
+            <input type="email" name="email" placeholder="Vaš e-mail" className="footer-input" />
+            </label>
+            <label>
+            <input type="number" name="phone" placeholder="Broj Telefona" className="footer-input" />
+            </label>
+            <label>
+            <textarea type="text" name="message" placeholder="Unesite vašu poruku" className="footer-input2"></textarea>
+            </label>
+            <label className="footer-btn">
+            <input type="submit" className="btn" value="POŠALJI UPIT" />
+            </label>
+          </form>
+        </div>
+        </section> 
+        <section className="footer-info">
+        <div >
+         <h3 className="footer-info-logo" style={{color: "white"}}>OPG Goluža</h3>
+         <div className="footer-info-area">
+          <h5 className="footer-info-cont" style={{color: "white"}}>Email: neki@email.com</h5>
+          <h5 className="footer-info-cont" style={{color: "white"}}>Telefon: 09XXXXXXXX</h5>
+         </div>
+        </div>
+      </section>
+          </div>
+    );
+  }
+
+
+
+/* function Footer (){
+
   return (
     <div className="footer-container">
       <section className="footer-subscription">
@@ -16,36 +72,33 @@ function Footer() {
         Ukoliko imate bilo kakvih upita slobodno nam se obratite putem naše kontakt forme 
         </p>
         <div className="input-areas">
-          <form className="footer_input" onSubmit={sendEmail} >
-            <input type="name" name="name" placeholder="Vaše ime"
-            className="footer-input"/>
-             <input type="email" name="email" placeholder="Vaš e-mail"
-            className="footer-input"/>
-             <input type="txt" name="phone" placeholder="Vaš telefon"
-            className="footer-input"/>
+          <form className="footer_input" >
+            <input type="name" name="name" placeholder="Vaše ime" className="footer-input" />
+            <input type="email" name="email" placeholder="Vaš e-mail" className="footer-input" />
+            <input type="txt" name="phone" placeholder="Vaš telefon" className="footer-input" />
           </form>
           <form>
-          <input type="txt" cols="30" rows="25" name="message" placeholder ="Unesite vašu poruku"
-            className="footer-input2" />
+          <input type="txt" name="message" placeholder ="Unesite vašu poruku" className="footer-input2" />
           </form>
           <div className="footer-btn">
-        <Button className="btns" buttonStyle="btn--outline" buttonSize="btn--large">
+        <button className="btns" buttonStyle="btn--outline" buttonSize="btn--large">
           POŠALJI UPIT
-        </Button>
-        </div>
+          </button>
+         </div>
         </div>
       </section>
       <section className="footer-info">
         <div >
          <h3 className="footer-info-logo" style={{color: "white"}}>OPG Goluža</h3>
-         <div className="footer-info-area"><h5 className="footer-info-cont" style={{color: "white"}}>Email: neki@email.com</h5>
-         <h5 className="footer-info-cont" style={{color: "white"}}>Telefon: 09XXXXXXXX</h5>
+         <div className="footer-info-area">
+          <h5 className="footer-info-cont" style={{color: "white"}}>Email: neki@email.com</h5>
+          <h5 className="footer-info-cont" style={{color: "white"}}>Telefon: 09XXXXXXXX</h5>
          </div>
         </div>
       </section>
     </div>
   )
-}
+};
 
-export default Footer
+export default Footer; */
 
